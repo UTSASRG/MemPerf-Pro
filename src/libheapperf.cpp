880f6afc63d45cfeaa54f424ddbb1c8d1d8a8fa4
Sam Silvestro <ssilvestro@gmail.com>
2016-04-28 16:07:37 -0500

    Added two-level hashmap to track mem allocation callsites and print them after program finishes.

--- src/libheapperf.cpp
+++ src/libheapperf.cpp
@@ -31,7 +31,8 @@ __thread bool insideHashMap = false;
 
 __thread thread_t * current;
 xpheap<xoneheap<xheap>> xmemory::_pheap;
-thread_local std::unordered_map<void*, std::tuple<int, int>> u;
+//thread_local std::unordered_map<void*, std::tuple<int, int>> memAllocCountMap;
+thread_local std::unordered_map<void*, std::unordered_map<void*, tuple<int, int>>> memAllocCountMap;
 
 __attribute__((constructor)) void initializer() {
 	Real::initializer();	
@@ -43,6 +44,7 @@ __attribute__((destructor)) void finalizer() {
 
 typedef int (*main_fn_t)(int, char**, char**);
 main_fn_t real_main;
+extern "C" void printHashMap();
 
 // Doubletake's main function
 int doubletake_main(int argc, char** argv, char** envp) {
@@ -52,8 +54,10 @@ int doubletake_main(int argc, char** argv, char** envp) {
 
 	initSampling();
 
-  // Call the program's main function
-  return real_main(argc, argv, envp);
+	int main_ret_val = real_main(argc, argv, envp);
+	printHashMap();
+
+  return main_ret_val;
 }
 
 extern "C" int __libc_start_main(main_fn_t, int, char**, void (*)(), void (*)(), void (*)(), void*) __attribute__((weak, alias("doubletake_libc_start_main")));
@@ -90,36 +94,56 @@ extern "C" {
   	void* caller_address;/* the address of caller */
 	};
 
+	extern void *__libc_stack_end;
+
   void* xxmalloc(size_t sz) {
     void* ptr = NULL;
 
-		printf("inside xxmalloc(%ld), initialized=%d \n", sz, initialized);
+		//printf("inside xxmalloc(%ld), initialized=%d, insideHashMap=%d\n", sz, initialized, insideHashMap);
+
 		if(initialized && !insideHashMap) {
 			insideHashMap = true;
-			void *callsite = __builtin_extract_return_addr(__builtin_return_address(1));
 
-			std::unordered_map<void*, std::tuple<int, int>>::iterator search = u.find(callsite);
-			if(search != u.end()) {
-				std::tuple<int, int> found = search->second;
-				int oldCount = std::get<0>(found);
-				int oldSize = std::get<1>(found);
-				//printf("found existing callsite: %p! old count =  %d; old allocation size = %d; old map size = %lu\n", callsite, oldCount, oldSize, u.size());
-				search->second = std::make_tuple((oldCount+1), (oldSize+sz));
-			} else {
-				//printf("adding new callsite: %p! old map size = %lu\n", callsite, u.size());
+			struct stack_frame *current_frame;
+			current_frame = (struct stack_frame*)(__builtin_frame_address(0));
+			void *callsite1 = current_frame->caller_address;
+			current_frame = current_frame->prev;
+			void *callsite2 = current_frame->caller_address;
+
+			//printf(">>> debug: callsite1, callsite2 == %p, %p\n", callsite1, callsite2);
+
+			std::unordered_map<void*, std::unordered_map<void*, tuple<int, int>>>::iterator search_level1 =
+							memAllocCountMap.find(callsite1);
+	
+			// If we found a match on callsite1...
+			if(search_level1 != memAllocCountMap.end()) {
+				//printf(">>> found key=%p in level 1 map\n", callsite1);
+				std::unordered_map<void*, tuple<int, int>> found_level1 = search_level1->second;
+				std::unordered_map<void*, tuple<int, int>>::iterator search_level2 = found_level1.find(callsite2);
+
+				// If we found a match on callsite2...
+				if(search_level2 != found_level1.end()) {
+					//printf(">>> found key=%p in level 2 map for key=%p in level 1\n", callsite2, callsite1);
+					std::tuple<int, int> found_level2 = search_level2->second;
+					int oldCount = std::get<0>(found_level2);
+					int oldSize = std::get<1>(found_level2);
+					//search_level2->second = std::make_tuple((oldCount+1), (oldSize+sz));
+					memAllocCountMap[callsite1][callsite2] = std::make_tuple((oldCount+1), (oldSize+sz));
+				} else {	// If we did NOT find a match on callsite2...
+					//printf(">>> did NOT find key=%p in level 2 map for key=%p in level 1\n", callsite2, callsite1);
+					std::tuple<int, int> new_tuple(std::make_tuple(1, sz));
+					//found_level1.insert({callsite2, new_tuple});
+					memAllocCountMap[callsite1].insert({callsite2, new_tuple});
+				}
+			} else {	// If we did NOT find a match on callsite1...
+				//printf(">>> did NOT find key=%p in level 1 map\n", callsite1);
+				std::unordered_map<void*, tuple<int, int>> new_level2_map;
 				std::tuple<int, int> newTuple(std::make_tuple(1, sz));
-				u.insert({callsite, newTuple});
+				new_level2_map.insert({callsite2, newTuple});
+				memAllocCountMap.insert({callsite1, new_level2_map});
 			}
-			printf("Hash map updated; new contents:\n");
-			for(const auto& n : u) {
-				std::tuple<int, int> tuple = n.second;
-				int count = std::get<0>(tuple);
-				int size = std::get<1>(tuple);
-				printf("Key:[%p] Value:[(%d, %d)]\n", n.first, count, size);
-			}
-			printf("\n");
+			insideHashMap = false;
 		}
-		insideHashMap = false;
 
 		if(sz == 0) {
 			sz = 1;
@@ -144,6 +168,26 @@ extern "C" {
 		//fprintf(stderr, "MEMORY allocation with ptr %p to %lx\n", ptr, ((intptr_t)ptr + sz));
     return ptr;
   }
+
+	void printHashMap() {
+		printf("Hash map contents:\n");
+		for(const auto& level1_entry : memAllocCountMap) {
+			bool isFirstEntry = true;
+			void *callsite1 = level1_entry.first;
+			std::unordered_map<void*, tuple<int, int>> level2_map = level1_entry.second;
+			for(const auto& level2_entry : level2_map) {
+				void *callsite2 = level2_entry.first;
+				std::tuple<int, int> tuple = level2_entry.second;
+				int count = std::get<0>(tuple);
+				int size = std::get<1>(tuple);
+				if(isFirstEntry)
+					printf("   (%-18p, (%p, (%d, %d)))\n", callsite1, callsite2, count, size);
+				else
+					printf("   (                  , (%p, (%d, %d)))\n", callsite2, count, size);
+				isFirstEntry = false;
+			}
+		}
+	}
 
   void xxfree(void* ptr) {
     if(initialized && ptr) {
