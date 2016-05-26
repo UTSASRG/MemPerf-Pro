7910c4772b14788b4aef2ba5623f55810970d1d6
Sam Silvestro <ssilvestro@gmail.com>
2016-05-25 23:29:20 -0500

    Modified Makefile to include dependency for test/alloc.cpp; cleanup of alloc.cpp

--- src/test/alloc.cpp
+++ src/test/alloc.cpp
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
 
 int main() {
 	int i;
-	int *ptr;
+	int * ptr;
 
-	printf("In the beginning of main\n");
+	printf("alloc.cpp: in the beginning of main\n");
 
 	for(i  = 0; i < 10; i++) {
-		ptr = (int *) malloc(10 * sizeof(int));
+		ptr = (int *) malloc(sizeof(int));
 		*ptr = i;
 		free(ptr);
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
