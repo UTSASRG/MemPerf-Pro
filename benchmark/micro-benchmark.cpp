7db0ec286d8db06569cd8699af4623fde570a8a3
XIN ZHAO <xinzhao@XINdeMacBook-Pro.local>
2020-05-12 11:04:31 -0500

    time adjust

--- benchmark/micro-benchmark.cpp
+++ benchmark/micro-benchmark.cpp
@@ -54,7 +54,7 @@ int random_pause_max = 50;
 double cpu_cycles_per_second = 2094895684.64;
 
 double cycles_per_allocation = cpu_cycles_per_second / allocationPerSeconds;
-double cycles_per_pause = 382;
+double cycles_per_pause = 25;
 
 
 class Foo {
@@ -98,10 +98,10 @@ void new_allocation_worker() {
     for (int i = 0; i < niterations; i++) {
         random_pause();
         for (int j = 0; j < nobjects; j++) {
+            fprintf(stderr, "new allocate num:%d, time:%lf \n", j, rdtscp() / cpu_cycles_per_second);
             unsigned long long start = rdtscp();
             total_new_allocations[i * nobjects + j] = new Foo[objSize];
-            fprintf(stderr, "new allocate num:%d, time:%llu \n", j, rdtscp());
-            assert (total_new_allocations[j]);
+            assert (total_new_allocations[i * nobjects + j]);
             rate_limit(rdtscp() - start);
         }
     }
