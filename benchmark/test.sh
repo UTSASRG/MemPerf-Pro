ba79b04b7f0967450d259873c9d981c7c3eedfbd
xinnnnzhao <xinnnnzhao@gmail.com>
2020-05-12 15:58:20 -0400

    benchmark test scripts

--- benchmark/test.sh
+++ benchmark/test.sh
@@ -9,9 +9,9 @@ for allocator in {"tcmalloc","jemalloc"}
 do
 	for object_size in {9,4099}
 	do
-		for allocation_per_second in {1,}
+		for allocation_per_second in {100,1000,5000,10000,20000}
 		do
-			/micro-$allocator $threads $object_size $allocation_per_second
+			./micro-$allocator $threads $object_size $allocation_per_second
 		done
 	done
 done
