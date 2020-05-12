d984fe0388ed78f42bdcc73f7c7608a17eefae23
XIN ZHAO <xinzhao@XINdeMacBook-Pro.local>
2020-05-12 14:32:01 -0500

    adjust

--- benchmark/micro-benchmark.cpp
+++ benchmark/micro-benchmark.cpp
@@ -208,7 +208,7 @@ int main(int argc, char *argv[]) {
     total_free_allocation_cycles = (unsigned long long *) malloc(nthreads * sizeof(unsigned long long));
     total_free_deallocation_cycles = (unsigned long long *) malloc(nthreads * sizeof(unsigned long long));
 
-    printf("Running micro benchmark for %d threads, %d objSize, %d allocationPerSeconds, %lf double CYCLES_PERSECOND = 2094895684.64; , %lf CYCLES_PER_PAUSE, %d nobjects, %d iterations...\n",
+    printf("Running micro benchmark for %d threads, %d objSize, %d allocationPerSeconds, %lf CYCLES_PERSECOND , %lf CYCLES_PER_PAUSE, %d nobjects, %d iterations...\n",
            nthreads, objSize,
            allocationPerSeconds, CYCLES_PERSECOND, CYCLES_PER_PAUSE, nobjects, niterations);
 
